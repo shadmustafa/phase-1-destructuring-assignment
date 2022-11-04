2524b750b3f95f7af664637e610a431a56f543ef
shad mustafa <shadmanmi2018@gmail.com>
2022-11-04 20:00:07 +0300

    Done

--- index.js
+++ index.js
@@ -1,6 +1,23 @@
 const farmAnimals = 'cow horse sheep pig chicken';
+const[moo,neigh,baa,oink,cluck ] = farmAnimals.split(' ');
+const[bessie, ,dolly,babe,little]=farmAnimals.split(' ');
+const[blackAndWhite, ,black,pink,]=farmAnimals.split(' ');
+
+
+
+
+
+
+
+
+
+
 
 const colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'];
+
+const[red, orange, yellow, green, blue, indigo, violet]=colors
+const[r, o, y, g, b, , v]=colors
+const[, , , , , indg, t]=colors
 
 const muppet = {
   muppetName: 'Miss Piggy',
@@ -9,6 +26,7 @@ const muppet = {
   job: 'Cast member of The Muppet Show',
   partner: 'Kermit'
 };
+const { muppetName, color,song ,  job,  partner} = muppet;
 
 const nestedMuppet = {
   nestedName: 'Kermit',
@@ -24,6 +42,10 @@ const nestedMuppet = {
   nestedJob: 'Host of The Muppet Show',
   nestedPartner: 'Miss Piggy'
 };
+
+const {album:{theMuppetMovie:{song2,song4}}, nestedJob, nestedPartner} = nestedMuppet;
+
+
 
 // Strings
 
